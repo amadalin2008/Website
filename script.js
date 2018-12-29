0ac550fdefc7efe832c770489c892216142a3877
amadalin2008 <46243059+amadalin2008@users.noreply.github.com>
2018-12-29 14:17:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function button2() {
+    alert("Nu exista noutati!");
+}
+function button3() {
+    alert("Esti deja la pagina video!")
+}
+function button4() {
+    alert("Esti deja la pagina Poze!")
+}
